451c71acd1ee40af598ebdedcd5a26a9155315af
jose arjona <josedarjona@gmail.com>
2018-05-23 11:31:03 -0400

    backend returns in frontend

--- routes/search-route.js
+++ routes/search-route.js
@@ -12,70 +12,133 @@ const axios = require("axios");
 
 
 router.post('/gymsearch', (req, res, next) => {
-
-  // console.log("check this !!!--->", req.body.search)
-
-  // const search = 'equinox brickell';
-  // console.log("req.body ???????????????????????????", req.body)
-  const search = req.body.search;
-  // console.log('body whatever this is ----------', search)
   
 
-  axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${search}&type=gym&location=25.761681,-80.191788&radius=8050&key=AIzaSyCUertGINeIoS4nQ7zpyuJzqyUg1PhXXws`)
-  .then((result)=>{
-    // console.log("this is the result!!--->", result) //<<<just objects
-    // console.log("this is the data--->", result.data) //<<<gives info
-    // console.log(`place id --->${result.data.results[0].place_id}`)
+  console.log("req.body ???????????????????????????", req.body)
+  const search = req.body.searchTerm;
+  console.log('body whatever this is ----------', search)
+
+axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${search}&type=gym&location=25.761681,-80.191788&radius=8050&key=AIzaSyCUertGINeIoS4nQ7zpyuJzqyUg1PhXXws`)
+    .then((result)=>{
+      // console.log("this is the result!!--->", result) //<<<just objects
+      // console.log("this is the data--->", result.data) //<<<gives info
+      const data = [];
+  
+      const dataToSend = {
+        name:'',
+        place_id:'',
+        day:[],
+      };
+      
+      const idSearch = result.data.results;
+  
+      idSearch.forEach(id => {
+  
+  
+        dataToSend.name = id.name;
+        dataToSend.place_id = id.place_id;
+        placeID = id.place_id;
+  
+          busy_hours(placeID, 'AIzaSyCUertGINeIoS4nQ7zpyuJzqyUg1PhXXws' )
+          .then(busyhours => {
+            dataToSend.day.push(busyhours.week);
+            
+            console.log("something something something something", data)
+  
+            
+          });
+          
+          return data.push(Object.assign({},dataToSend));
+  
+          console.log("in the middle data console log ----->", data)
+          
+          
+        })
+  
+  
+        console.log("at end data console log---->" , data)  
+        
+        res.json(data)
+      })
+      .catch((err) => {
+        console.log(err)
+        next(err);
+      });
+    });
+
+
+  
+
+  module.exports = router; 
+
+  // console.log("check this !!!--->", req.body.search)
+  
+  // const search = 'equinox brickell';
+
+
+
+    // console.log("idsearch is: ---> ",idSearch)
+
+      // console.log(`place id --->${result.data.results[0].place_id}`)
     // console.log("information about results ==========================================")
     // console.log(result.data)
     // console.log("information about results ==========================================")
-    const idSearch = result.data.results;
-    idSearch.forEach(id =>{
-      // console.log(`*!*!*!*!*!**!!**!*!*!*!*!*!*!${idSearch}*!**!*!*!*!*!*!*!*!**!*!*!`);
-
+  
+        // console.log(`*!*!*!*!*!**!!**!*!*!*!*!*!*!${idSearch}*!**!*!*!*!*!*!*!*!**!*!*!`);
       // console.log("element???" + id.place_id);
       // console.log(`each placeID ${placeID}`);
-      placeID = id.place_id;
+  
+  
+  
+  
+  //console.log("data to send", dataToSend)
+      // dataToSend.name = id.name;
       // console.log(`each placeID ${placeID}`);
-        busy_hours(placeID, 'AIzaSyCUertGINeIoS4nQ7zpyuJzqyUg1PhXXws' )
-        .then(data => {
-        //  console.log('place id data ----------->', data); 
-
-         data.week.forEach(day=>{
-           dayOfWeek = day.day;
-          //  hoursOfDay = day.hours;
-          //  console.log('day of week ------>' + dayOfWeek);
-
-           day.hours.forEach(hours=>{
-
-
-              hourOfDay = hours.hour;
-              busyPercent = hours.percentage;
-              // console.log('time---->' + hourOfDay + ' ------ busy percent---->' + busyPercent)
-              
-
-           })
-
-         })
-         
-     });
-      
-    })
+  
+  // console.log (busyhours)
+//  console.log('place id data ----------->', data); 
+//  console.log("before: ", dataToSend.week.length);
+//  dataToSend.week.push(data.week)
+  
+  
+  
+  
+  // console.log("data to send in out: ", dataToSend)
+  
+  // result.data.results.forEach(element => {
     
-    // result.data.results.forEach(element => {
-
-      // console.log("element???" + element.place_id)
+    // console.log("element???" + element.place_id)
     //   return placeID = element.place_id;
     // });
-
     
-    res.json(idSearch)
-    })
-    .catch((err) => {
-      next(err);
-    });
+    
+    // res.json(idSearch)
+    
     // console.log(result.data)
-  })
+    
+    
+    //  console.log("data to send", dataToSend)
+     
+    // console.log('kajshdfkjhdasjkkjdhasf',data)
+
+    // console.log("after: ", dataToSend.week.length);
+    // res.json(dataToSend)
+
+  //  data.week.forEach(day=>{
+  //    dayOfWeek = day.day;
+     
+  //   //  hoursOfDay = day.hours;
+  //   //  console.log('day of week ------>' + dayOfWeek);
+  //    day.hours.forEach(hours=>{
+  //       hourOfDay = hours.hour;
+  //       busyPercent = hours.percentage;
+  //       // console.log('time---->' + hourOfDay + ' ------ busy percent---->' + busyPercent)
+
+  //    })
+
+  //  })
+   
+  // console.log("data to send in mid: ", dataToSend)
 
 
 
@@ -84,12 +147,3 @@ router.post('/gymsearch', (req, res, next) => {
 
 
 
-
-
-
-
-
-
-
-
-module.exports = router;
